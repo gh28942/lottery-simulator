adc9c65e21058e5a48c8e827bef30fb575014b0e
gh28942 <GerH@example.com>
2021-02-28 02:58:59 +0100

    Updates

---
+++ precache-manifest.3c1d26c3f9717b5e42afe58e316ebd71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a4adaa1f5138d19b3a209a46028432",
+    "url": "/lottery-simulator/austria/index.html"
+  },
+  {
+    "revision": "39398b36902ea85e3f48",
+    "url": "/lottery-simulator/austria/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "6ef142bb58eb263c2577",
+    "url": "/lottery-simulator/austria/static/css/main.74b59867.chunk.css"
+  },
+  {
+    "revision": "39398b36902ea85e3f48",
+    "url": "/lottery-simulator/austria/static/js/2.ac88e3d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery-simulator/austria/static/js/2.ac88e3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef142bb58eb263c2577",
+    "url": "/lottery-simulator/austria/static/js/main.8f8accf7.chunk.js"
+  },
+  {
+    "revision": "0f316d8428ab6dac6a0d",
+    "url": "/lottery-simulator/austria/static/js/runtime-main.26faf091.js"
+  }
+]);
