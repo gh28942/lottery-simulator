7324cdb35770723bb742d83df8af57dc0b7d9b65
gh28942 <GerH@example.com>
2021-02-28 02:13:56 +0100

    Updates

---
+++ precache-manifest.fd7e5a33ffea1ac6e87da8012e8fafe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6558a50c43023085dab92f98157397eb",
+    "url": "/lottery-simulator/index.html"
+  },
+  {
+    "revision": "39398b36902ea85e3f48",
+    "url": "/lottery-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "939d774d0eabe5c65c2d",
+    "url": "/lottery-simulator/static/css/main.74b59867.chunk.css"
+  },
+  {
+    "revision": "39398b36902ea85e3f48",
+    "url": "/lottery-simulator/static/js/2.ac88e3d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery-simulator/static/js/2.ac88e3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "939d774d0eabe5c65c2d",
+    "url": "/lottery-simulator/static/js/main.52908fde.chunk.js"
+  },
+  {
+    "revision": "6be4a582884dcb689521",
+    "url": "/lottery-simulator/static/js/runtime-main.896393e3.js"
+  }
+]);
