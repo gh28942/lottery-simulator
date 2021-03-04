8b3587334326a1fd780f79ad32054d7a616265f7
gh28942 <GerH@example.com>
2021-03-04 19:49:54 +0100

    Updates

---
+++ precache-manifest.3750d7681476250b3694d0173cae5b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f92c06b22ed2ca3203b1c34c712c6fb",
+    "url": "/lottery-simulator/index.html"
+  },
+  {
+    "revision": "b98706d0ce31c511b48d",
+    "url": "/lottery-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "c6987ace668a85bf1c4b",
+    "url": "/lottery-simulator/static/css/main.cb471b56.chunk.css"
+  },
+  {
+    "revision": "b98706d0ce31c511b48d",
+    "url": "/lottery-simulator/static/js/2.2b24b4d6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/lottery-simulator/static/js/2.2b24b4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6987ace668a85bf1c4b",
+    "url": "/lottery-simulator/static/js/main.57d11e25.chunk.js"
+  },
+  {
+    "revision": "6be4a582884dcb689521",
+    "url": "/lottery-simulator/static/js/runtime-main.896393e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lottery-simulator/precache-manifest.dfcba7dc6bf0182c86cb1eca5205ab1a.js"
+  "/lottery-simulator/precache-manifest.3750d7681476250b3694d0173cae5b9c.js"
 );
 
 self.addEventListener('message', (event) => {
